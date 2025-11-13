95d876e2df21331896dcba344ac70ba5addf9590
Crislany Victória <crislanyviictoria@gmail.com>
2025-11-12 21:49:13 -0300

    estrutura da classe Musica

---
+++ Musica.java
@@ -0,0 +1,46 @@
+public class Musica {
+    private int id;
+    private String titulo;
+    private String artista;
+    private int duracao;
+    private String genero;
+
+    public Musica(int id, String titulo, String artista, int duracao, String genero){
+        this.id = id;
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracao = duracao;
+        this.genero = genero;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public String getArtista(){
+        return artista;
+    }
+
+    public int getDuracao(){
+        return duracao;
+    }
+
+    public String getGenero(){
+        return genero;
+    }
+
+    @Override
+    public String toString(){
+        return "Musica{" +
+                "id=" + id+
+                ", titulo='" + titulo + '\''+
+                ", artista='" + artista + '\'' +
+                ", duracao=" + duracao +
+                ", genero='" + genero + '\''+
+                '}';
+    }
+}
