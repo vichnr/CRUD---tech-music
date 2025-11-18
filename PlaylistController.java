9e937bd8509d3d6d11c61cd9aea34dd2c9ef6d67
Crislany Victória <crislanyviictoria@gmail.com>
2025-11-17 21:56:23 -0300

    classe responsável para controlar playlists

---
+++ PlaylistController.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaylistController{
+    private List<Playlist>playlists;
+    private int nextId;
+
+    public PlaylistController(){
+        this.playlists=new ArrayList<>();
+        this.nextId =1;
+    }
+
+    public Playlist criarPlaylist(String nome){
+        Playlist playlist= new Playlist(nextId,nome);
+        playlists.add(playlist);
+        nextId++;
+        return playlist;
+    }
+
+    public void adicionarMusica(int playlistId,Musica musica){
+        Playlist playlist= buscarPlaylistPorId(playlistId);
+        if (playlist!= null){
+            playlist.adicionarMusica(musica);
+        }
+    }
+
+    public boolean removerMusicaPorPosicao(int playlistId, int posicao){
+        Playlist playlist= buscarPlaylistPorId(playlistId);
+        if (playlist!= null && posicao>= 0 && posicao <playlist.getMusicas().size()){
+            Musica musica= playlist.getMusicas().get(posicao);
+            playlist.removerMusica(musica);
+            return true;
+        }
+        return false;
+    }
+
+    public Playlist buscarPlaylistPorId(int id){
+        for (Playlist playlist: playlists) {
+            if (playlist.getId()== id) {
+                return playlist;
+            }
+        }
+        return null;
+    }
+    public List<Playlist> listarPlaylists(){
+        return new ArrayList<>(playlists);
+    }
+
+
+}
